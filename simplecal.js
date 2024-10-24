72b9f2e611daeca242b3d07efaa94c628e731fc3
Sravyasusmitha <kambhamsravyasusmitha@gmail.com>
2024-10-24 16:46:36 +0530

    Regex corrected

--- simplecal.js
+++ simplecal.js
@@ -1,14 +1,8 @@
 function add(numbers) {
     if (!numbers) return 0;
 
-    let delimiter = /,||\n/;
-    
-    // Custom delimiter
-    if (numbers.startsWith("//")) {
-        const delimiterEnd = numbers.indexOf("\n");
-        delimiter = new RegExp(numbers.substring(2, delimiterEnd));
-        numbers = numbers.substring(delimiterEnd + 1);
-    }
+    // Delimiter is a comma or newline
+    let delimiter = /,|\\n/;
 
     const numberArray = numbers.split(delimiter);
     let sum = 0;
@@ -17,9 +11,12 @@ function add(numbers) {
     numberArray.forEach(number => {
 								  
         const num = parseInt(number);
-        if (isNaN(num)) return;
-        if (num < 0) negatives.push(num);
-        sum += num;
+        if (isNaN(num)) 
+            return
+        else if (num < 0) 
+            negatives.push(num)
+            //throw new Error('negavtive')
+        else sum += num;
     });
 
     if (negatives.length > 0) {
